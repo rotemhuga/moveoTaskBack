43ad6b9c5a67b532cd0ba2c5f4810c20ef33b3ca
rotem huga <rotem.3007@gmail.com>
2023-04-22 13:59:51 +0300

    update the fetch - 22.04

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://moveotasksback.onrender.com",
+    origin: "https://moveotaskfront.onrender.com",
     methods: ["GET", "POST"],
   },
 });
